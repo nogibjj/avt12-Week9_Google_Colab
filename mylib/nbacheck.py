c310e8a748aafd8868a734fa41205f2827e3849d
Atreya Tadepalli <atreya.tadepalli@duke.edu>
2024-09-15 21:58:13 -0400

    Updates

--- mylib/nbacheck.py
+++ mylib/nbacheck.py
@@ -2,10 +2,8 @@
 library file
 '''
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 data1="NBA_Data.csv"
 
@@ -13,30 +11,30 @@ def load_dataset(data):
     df=pd.read_csv(data)
     return df
 
-def grab_mean(data,col):
-    return data[col].mean()
+#def grab_mean(data,col):
+    #return data[col].mean()
 
-def create_histogram(data,col):
-    data[col].hist()
-    plt.xlabel('Values')
-    plt.ylabel('Frequency')
-    plt.title('Basic Histogram')
+#def create_histogram(data,col):
+    #data[col].hist()
+    #plt.xlabel('Values')
+    #plt.ylabel('Frequency')
+    #plt.title('Basic Histogram')
 
 
 def nba_summary(data):
     return data.describe()
 
-def heat_map(data,col1,col2):
-    return 
+#def heat_map(data,col1,col2):
+    #return 
 
-def grab_median(data,col):
-    return data[col].median()
+#def grab_median(data,col):
+    #return data[col].median()
 
-def grab_stdev(data,col):
-    return data[col].stdev()
+#def grab_stdev(data,col):
+    #return data[col].stdev()
 
-def grab_max(data,col):
-    return data[col].max()
+#def grab_max(data,col):
+    #return data[col].max()
 
 def histogram_3ptfg(data):
     plt.figure(figsize=(10,6))
