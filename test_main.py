f0a887bb2606d13d56188ce962f8fc0462df4eda
GitHub Action <action@github.com>
2024-09-16 03:24:58 +0000

    Prepare report

--- test_main.py
+++ test_main.py
@@ -1,20 +1,21 @@
-from main import g_describe, grab_mean, grab_median,grab_stdev
+from main import g_describe, grab_mean, grab_median, grab_stdev
 import pandas as pd
 
-test_file="NBA_Data.csv"
-df=pd.read_csv(test_file)
-print(grab_mean(df,"AST"))
-print(grab_median(df,"AST"))
-print(grab_stdev(df,"AST"))
+test_file = "NBA_Data.csv"
+df = pd.read_csv(test_file)
+print(grab_mean(df, "AST"))
+print(grab_median(df, "AST"))
+print(grab_stdev(df, "AST"))
+
 
 def test_describe():
     # testing out nba_summary
-    test_summary=g_describe(test_file)
-    assert test_summary.shape == (8,27)
-    assert grab_mean(df,"AST") == 99.86530612244898
-    assert grab_median(df,"AST") == 54.0
-    assert grab_stdev(df,"AST") == 124.65415734967988
-    
+    test_summary = g_describe(test_file)
+    assert test_summary.shape == (8, 27)
+    assert grab_mean(df, "AST") == 99.86530612244898
+    assert grab_median(df, "AST") == 54.0
+    assert grab_stdev(df, "AST") == 124.65415734967988
+
 
 if __name__ == "__main__":
     test_describe()
