13bbac15eeae1526546cbc5f624b3476d3791cdf
Atreya Tadepalli <atreya.tadepalli@duke.edu>
2024-09-15 22:33:56 -0400

    test main updated

--- test_main.py
+++ test_main.py
@@ -1,11 +1,21 @@
-from main import multiply
+from main import g_describe, save_to_md
+
+test_file="NBA_Data.csv"
+
+def test_describe():
+    # testing out nba_summary
+    test_summary=g_describe(test_file)
+    assert test_summary.shape == (8,27)
+    
+
+def test_markdown():
+    #converts to markdown
+    save_to_md(test_file)
 
 
-def test_multiply():
-    # testing out multiply function
-    assert multiply(4, 6) == 24
-    assert multiply(17, 12) == 204
+
 
 
 if __name__ == "__main__":
-    test_multiply()
+    test_describe()
+    test_markdown()
