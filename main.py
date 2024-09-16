b162b838c3c101607c550c31c2caa2cf100c1c14
Atreya Tadepalli <atreya.tadepalli@duke.edu>
2024-09-15 22:39:11 -0400

    histogram method updated

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ def load_dataset(data):
 def nba_summary(data):
     return data.describe()
 
-def histogram_3ptfg(data):
+def histogram_ast(data):
     plt.figure(figsize=(10,6))
-    plt.hist(data["3P%"], bins=20)
-    plt.title("3 Point Percentage in 2023-24 Season-1")
+    plt.hist(data["AST"], bins=20)
+    plt.title("Assists in 2023-24 Season-1")
     plt.xlabel("NBA Players")
     plt.ylabel("%")
-    plt.savefig("3_pt_data.png")
+    plt.savefig("assists.png")
     return plt.show()
 
 def bar_chart_points(data):
     df=pd.read_csv(data)
     plt.figure(figsize=(10,6))
-    plt.bar(df["Pos"],df["PTS"],color ='maroon', 
+    plt.bar(df["Pos"],df["AST"],color ='maroon', 
         width = 0.4)
     plt.savefig("points_by_position.png")
     plt.show()
@@ -34,8 +34,8 @@ def g_describe(data):
 histogram_3ptfg(data1)
 bar_chart_points(data1)
 
-def save_to_md():
-    test=g_describe(data1)
+def save_to_md(data):
+    test=g_describe(data)
     with open("nbastats.md","a", encoding='utf-8') as file:
         file.write("Describe:\n")
         file.write(test)
